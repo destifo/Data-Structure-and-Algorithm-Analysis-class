b4264ef9e328ff3064eb57a7d744786affa34cf3
destifo <estifanosbireda@gmail.com>
2021-09-22 22:15:50 +0300

    Elementary Data structures Implementation

--- stack with linked list/Stack_LinkedList.py
+++ stack with linked list/Stack_LinkedList.py
@@ -1,22 +1,30 @@
+from node import Node
+
+
 class StackedLinkedList:
     def __init__(self):
         self.head = None
 
     def isEmpty(self):
-        if self.head == None:
+        if self.head is None:
             return True
         return False
+
     def insert(self, value):
         node = Node(value)
-        if isEmpty():
+        if self.isEmpty():
             self.head = node
         else:
             temp = self.head
             self.head = node
             node.next = temp
 
+    def top(self):
+        temp = self.head
+        return temp.data
+
     def pop(self):
-        if isEmpty():
+        if self.isEmpty():
             return "Empty List"
         else:
             temp = self.head
--- stack with linked list/Test_the_DS.py
+++ stack with linked list/Test_the_DS.py
@@ -1,6 +1,11 @@
 from Arrays_In_Python import Array
+from Stack_LinkedList import StackedLinkedList
 from Stack_with_array import StackArray
 from stack import Stack
 
 x = StackedLinkedList()
+x.insert(5)
+x.insert(10)
+x.insert(15)
+print(x.top())
 
