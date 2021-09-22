a079a39cbafa1edcb07f3d43a1f64983d020279a
destifo <estifanosbireda@gmail.com>
2021-09-22 22:36:52 +0300

    added size method, implementing Stack using linked list

--- stack with linked list/Stack_LinkedList.py
+++ stack with linked list/Stack_LinkedList.py
@@ -4,6 +4,7 @@ from node import Node
 class StackedLinkedList:
     def __init__(self):
         self.head = None
+        self.last_index = -1
 
     def isEmpty(self):
         if self.head is None:
@@ -14,14 +15,20 @@ class StackedLinkedList:
         node = Node(value)
         if self.isEmpty():
             self.head = node
+            self.last_index += 1
         else:
             temp = self.head
             self.head = node
             node.next = temp
+            self.last_index += 1
 
     def top(self):
         temp = self.head
         return temp.data
+
+    def size(self):
+        if self.isEmpty(): return 0
+        return self.last_index + 1
 
     def pop(self):
         if self.isEmpty():
@@ -30,4 +37,5 @@ class StackedLinkedList:
             temp = self.head
             self.head = temp.next
             temp.next = None
+            self.last_index -= 1
             return temp.data
